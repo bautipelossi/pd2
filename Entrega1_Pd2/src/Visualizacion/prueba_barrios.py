64c063dcbcab0fb0233d91a409ad1d437e9125c7
Pabalo10 <158191335+Pabalo10@users.noreply.github.com>
2026-02-13 12:22:56 +0100

    Actualización y corrección
    Cambios en el código y depuración de este, comentarios añadidos

--- Entrega1_Pd2/src/Visualizacion/prueba_barrios.py
+++ Entrega1_Pd2/src/Visualizacion/prueba_barrios.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-# Rutas (Asegúrate que son las mismas que en tu script principal)
+# Rutas
 FHV_PATH = r"C:\Users\palon\Downloads\fhv_2023_clean.parquet"
 YLC_PATH = r"C:\Users\palon\Downloads\nyc_taxi_clean.parquet"
 ZONE_LOOKUP_URL = "https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv"
 
 def auditar_datos():
-    print("--- 1. Cargando datos ---")
+    print("1. Cargando datos")
     df_fhv = pd.read_parquet(FHV_PATH)
     df_ylc = pd.read_parquet(YLC_PATH)
     df_lookup = pd.read_csv(ZONE_LOOKUP_URL)
@@ -15,18 +15,18 @@ def auditar_datos():
     sample_fhv = df_fhv[['pulocationid']].head(1000).copy()
     sample_ylc = df_ylc[['pulocationid']].head(1000).copy()
     
-    print(f"\n--- 2. Tipos de datos detectados ---")
+    print(f"\n2. Tipos de datos detectados")
     print(f"FHV ID tipo: {sample_fhv['pulocationid'].dtype}")
     print(f"Taxi ID tipo: {sample_ylc['pulocationid'].dtype}")
     print(f"Lookup ID tipo: {df_lookup['LocationID'].dtype}")
 
-    # Intentar el merge tal cual lo tienes ahora
+    # Intentar el merge 
     df_combined = pd.concat([sample_fhv, sample_ylc])
     
     # MERGE
     merged = df_combined.merge(df_lookup, left_on='pulocationid', right_on='LocationID', how='left')
     
-    print("\n--- 3. Resultados del cruce (Borough) ---")
+    print("\n3. Resultados del cruce (Borough)")
     print(merged['Borough'].value_counts(dropna=False))
     
     nulos = merged['Borough'].isna().sum()
